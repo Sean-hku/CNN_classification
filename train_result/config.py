b8bdc9279fc544dc07b0342bb5ad7c7bcd2edc58
cheungbh <534660436@qq.com>
2020-08-09 21:37:49 +0800

    folder structure

--- train_result/config.py
+++ train_result/config.py
@@ -1,1 +1,5 @@
-models_name = "ceiling_2cls"
+folder_path = "train_result/ceiling_action/FrogFreestyle"
+folder_path = folder_path.replace("\\", "/")
+
+task_folder = folder_path.split("/")[-2]
+batch_folder = folder_path.split("/")[-1]
