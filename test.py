421690d6d5889883952d28328b1675d8093b7da2
cheungbh <534660436@qq.com>
2021-01-18 09:10:36 +0800

    fix test code, and adjust pretrain model structure

--- test.py
+++ test.py
@@ -10,39 +10,39 @@ from src.tester import ModelInference
 from src.utils import get_pretrain
 
 
-def test(model_path, img_path, batch_size, num_classes, keyword):
-    drown_ls = torch.FloatTensor([]).to("cuda:0")
-    stand_ls = torch.FloatTensor([]).to("cuda:0")
-    labels_ls_drown = torch.LongTensor([]).to("cuda:0")
-    labels_ls_stand = torch.LongTensor([]).to("cuda:0")
-    data_loader = DataLoader(img_path, batch_size)
-    pre_name = get_pretrain(model_path)
-    model = ModelInference(num_classes, pre_name, model_path, cfg='default')
-
-    pbar = tqdm(enumerate(data_loader.dataloaders_dict[keyword]), total=len(data_loader.dataloaders_dict[keyword]))
-    for i, (names, inputs, labels) in pbar:
-        inputs = inputs.to("cuda:0")
-        labels = labels.to("cuda:0")
-        outputs = model.CNN_model(inputs)
-        _, preds = torch.max(outputs, 1)
-        drown = torch.index_select(torch.sigmoid(outputs), 1, torch.tensor([1]).to("cuda:0"))
-        drown = drown.view(1, -1).squeeze()
-        stand = torch.index_select(torch.sigmoid(outputs), 1, torch.tensor([0]).to("cuda:0"))
-        stand = stand.view(1, -1).squeeze()
-        drown_ls = torch.cat((drown_ls, drown), 0)
-        stand_ls = torch.cat((stand_ls, stand), 0)
-        labels_ls_drown = torch.cat((labels_ls_drown, labels), 0)
-        labels_ls_stand = torch.cat((labels_ls_stand, torch.add(1, -labels).long()), 0)
-    precision, recall, thresholds = precision_recall_curve(labels_ls_drown.cpu().detach().numpy(),
-                                                           np.array(drown_ls.squeeze().cpu().detach().numpy()))
-    '''#for stand class
-    labels_ls_stand = labels_ls_stand.ge(-0.1)
-    precision, recall, thresholds = precision_recall_curve(labels_ls_stand.cpu().detach().numpy(),
-                                                   np.array(stand_ls.squeeze().cpu().detach().numpy()))
-                                                   '''
-    plt.figure(1)
-    plt.plot(precision, recall)
-    plt.show()
+# def test(model_path, img_path, batch_size, num_classes, keyword):
+#     drown_ls = torch.FloatTensor([]).to("cuda:0")
+#     stand_ls = torch.FloatTensor([]).to("cuda:0")
+#     labels_ls_drown = torch.LongTensor([]).to("cuda:0")
+#     labels_ls_stand = torch.LongTensor([]).to("cuda:0")
+#     data_loader = DataLoader(img_path, batch_size)
+#     pre_name = get_pretrain(model_path)
+#     model = ModelInference(num_classes, pre_name, model_path, cfg='default')
+#
+#     pbar = tqdm(enumerate(data_loader.dataloaders_dict[keyword]), total=len(data_loader.dataloaders_dict[keyword]))
+#     for i, (names, inputs, labels) in pbar:
+#         inputs = inputs.to("cuda:0")
+#         labels = labels.to("cuda:0")
+#         outputs = model.CNN_model(inputs)
+#         _, preds = torch.max(outputs, 1)
+#         drown = torch.index_select(torch.sigmoid(outputs), 1, torch.tensor([1]).to("cuda:0"))
+#         drown = drown.view(1, -1).squeeze()
+#         stand = torch.index_select(torch.sigmoid(outputs), 1, torch.tensor([0]).to("cuda:0"))
+#         stand = stand.view(1, -1).squeeze()
+#         drown_ls = torch.cat((drown_ls, drown), 0)
+#         stand_ls = torch.cat((stand_ls, stand), 0)
+#         labels_ls_drown = torch.cat((labels_ls_drown, labels), 0)
+#         labels_ls_stand = torch.cat((labels_ls_stand, torch.add(1, -labels).long()), 0)
+#     precision, recall, thresholds = precision_recall_curve(labels_ls_drown.cpu().detach().numpy(),
+#                                                            np.array(drown_ls.squeeze().cpu().detach().numpy()))
+#     '''#for stand class
+#     labels_ls_stand = labels_ls_stand.ge(-0.1)
+#     precision, recall, thresholds = precision_recall_curve(labels_ls_stand.cpu().detach().numpy(),
+#                                                    np.array(stand_ls.squeeze().cpu().detach().numpy()))
+#                                                    '''
+#     plt.figure(1)
+#     plt.plot(precision, recall)
+#     plt.show()
 
 
 def test2(model_path, img_path, batch_size, num_classes, keyword):
